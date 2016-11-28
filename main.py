3d5fffdbc32a7d4e5dc61c1b8b7b5258ebf502b0
nehirakdag <nehirakdag34@hotmail.com>
2016-11-28 00:26:59 -0500

    Registration implemented with error-checking/handling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import re
 
 from flask import Flask, render_template, request, session, g, redirect, url_for, abort, flash
 from flask_bootstrap import Bootstrap
@@ -8,38 +9,39 @@ from contextlib import closing
 app = Flask(__name__)
 app.config.from_object(__name__)
 Bootstrap(app)
+EMAIL_REGEX = re.compile(r"[^@\s]+@[^@\s]+\.[a-zA-Z0-9]+$")
 
 app.config.update(dict(
-	DATABASE=os.path.join(app.root_path, 'courseHelper.db'),
-	SECRET_KEY='comp307',
-	USERNAME='admin',
-	PASSWORD='default'
+    DATABASE=os.path.join(app.root_path, 'courseHelper.db'),
+    SECRET_KEY='comp307',
+    USERNAME='admin',
+    PASSWORD='default'
 ))
 #app.config.from_envvar('COURSEHELPER_SETTINGS', silent=True)
 
 @app.before_request
 def before_request():
-        g.db = connect_db()
-        print 'initialized DB'
+    g.db = connect_db()
+    print 'initialized DB'
 
 def connect_db():
-	# Connect to the database
-	#rv = sqlite3.connect(app.config['DATABASE'])
-	#rv.row_factory = sqlite3.Row
-	#return rv
-	return sqlite3.connect(app.config['DATABASE'])
+    # Connect to the database
+    #rv = sqlite3.connect(app.config['DATABASE'])
+    #rv.row_factory = sqlite3.Row
+    #return rv
+    return sqlite3.connect(app.config['DATABASE'])
 
 def get_db():
-	# Open a database connection if and only if there is none yet, for the current application context
-	if not hasattr(g, 'sqlite_db'):
-		g.sqlite_db = connect_db()
-	return g.sqlite_db
+    # Open a database connection if and only if there is none yet, for the current application context
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
 
 def init_db():
-	#db = get_db()
-	#with app.open_resource('schema.sql', mode='r') as f:
-	#	db.cursor().executescript(f.read())
-	#db.commit()
+    #db = get_db()
+    #with app.open_resource('schema.sql', mode='r') as f:
+    #   db.cursor().executescript(f.read())
+    #db.commit()
         with closing(connect_db()) as db:
                 with app.open_resource('schema.sql', mode='r') as f:
                     db.cursor().executescript(f.read())
@@ -50,66 +52,117 @@ if not(os.path.exists(app.config['DATABASE'])):
         
 #@app.cli.command('initdb')
 #def initdb_command():
-	# Initializes the database
-#	init_db()
-#	print 'Initialized the database according to the schema'
+    # Initializes the database
+#   init_db()
+#   print 'Initialized the database according to the schema'
 
 # Functions marked with teardown_appcontext are called every time the app context tears down
 # A teardown happens either when after everything went well (app closes, error = None) or an exception occurred
-@app.teardown_appcontext
-def close_db(error):
-	# Closes the database again at the end of the request
-	if hasattr(g, 'sqlite_db'):
-		g.sqlite_db.close()
-	print ("db closed")
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+    print ("db closed")
 
 @app.route('/')
 def hello_world():
-        print 'hello'
-	return render_template("index.html")
+    print 'hello'
+    return render_template("index.html")
 
 @app.route('/register')
 def registration():
-	return render_template("register.html")
+    return render_template("register.html")
 
-@app.route('/profiles')
-def show_entries():
-	db = get_db()
-	cur = db.execute('select username, password from entries order by id desc')
-	entries = cur.fetchall()
-	return render_template('profiles.html', entries=entries)
+# @app.route('/profiles')
+# def show_entries():
+#     db = get_db()
+#     cur = db.execute('select username, password from entries order by id desc')
+#     entries = cur.fetchall()
+#     return render_template('profiles.html', entries=entries)
+
+def checkForCorrectRegistration(username, password, passwordConf, email):
+    error = None
+
+    # Check if any of the fields were blank
+    if not username.strip():
+        error = "Error! Username can not be blank!"
+    elif not password.strip() or not passwordConf.strip():
+        error = "Error! Password can not be blank!"
+    elif not email.strip():
+        error = "Error! Email address can not be blank!"
+    # Check if the two password entries match
+    elif password != passwordConf:
+        error = "Error! Password must match Password Confirmation"
+    # Check if a valid email address was entered
+    elif not EMAIL_REGEX.match(request.form['email']):
+        error = "Error! Please input a valid email address"
+
+    return error
 
 @app.route('/add', methods=['GET', 'POST'])
-def add_entry():
+def register_to_db():
+    #error = None
     if session.get('logged_in'):
-    	flash('aborting!')
+        flash('aborting!')
         abort(401)
-    #db = get_db()
-    g.db.execute('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [request.form['user'] , request.form['email'], request.form['pass']])
-    g.db.commit()
+    db = get_db()
+
+    # print request.form['pass']
+    # print request.form['pwConf']
+    # Check if password entries match
+    # if request.form['pass'] != request.form['pwConf']:
+    #     error = "Error! Password must match Password Confirmation"
+    #     print error
+    #     return render_template("register.html", error=error)
+
+    # # Check if a valid email address was entered
+    # if not EMAIL_REGEX.match(request.form['email']):
+    #     error = "Error! Please input a valid email address"
+    #     print error
+    #     return render_template("register.html", error=error)
+    userName = str(request.form['user'])
+    passwd = str(request.form['pass'])
+    passwdConf = str(request.form['pwConf'])
+    email = str(request.form['email'])
+    
+    error = checkForCorrectRegistration(userName, passwd, passwdConf, email)
+    if not error is None:
+        print error
+        return render_template("register.html", error=error)
+
+    #Check if entered username was unique
+    try:
+        db.execute('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [userName , email, passwd])
+        db.commit()
+    #if not, redirect user to registration page
+    except sqlite3.IntegrityError:
+        db.rollback()
+        error = "Invalid Username! Please specify a unique username"
+        print error
+        return render_template("register.html", error=error)
+
     #print ('New entry was successfully posted')
-    return render_template("loggedin_home.html", username=str(request.form['user']))
+    return render_template("loggedin_home.html", username=userName)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['user'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['pass'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        elif request.form['pass'] != request.form['pwConf']:
-        	error = 'Password and confirmation must match'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if request.form['user'] != app.config['USERNAME']:
+#             error = 'Invalid username'
+#         elif request.form['pass'] != app.config['PASSWORD']:
+#             error = 'Invalid password'
+#         elif request.form['pass'] != request.form['pwConf']:
+#             error = 'Password and confirmation must match'
+#         else:
+#             session['logged_in'] = True
+#             flash('You were logged in')
+#             return redirect(url_for('show_entries'))
+#     return render_template('login.html', error=error)
 
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
-
-app.run(debug=True)
+# @app.route('/logout')
+# def logout():
+#     session.pop('logged_in', None)
+#     flash('You were logged out')
+#     return redirect(url_for('show_entries'))
